50852e40844fe0d090a753ef8e48ba4288bbb7fe
jace-kline <jace_kline@ku.edu>
2019-10-26 13:33:49 -0500

    Embedding PHP is not working

--- exercise6/FillDropdown.php
+++ exercise6/FillDropdown.php
@@ -1,6 +1,7 @@
 <?php
-$mysqli = new mysqli("mysql.eecs.ku.edu", "jace_kline", "feuta7Ya", "jace_kline");
 
+$mysqli = new mysqli("mysql.eecs.ku.edu", "jace_kline", "feuta7Ya", "jace_kline");
+            
 /* check connection */
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
@@ -10,18 +11,14 @@ if ($mysqli->connect_errno) {
 $query = "SELECT user_id FROM Users";
 
 if ($result = $mysqli->query($query)) {
-    echo "<h1><u>List of Users:</u></h1>";
-    echo "<table style='font-size:30px'>";
+    /* fetch associative array and display user names in the dropdown list*/
 
-    /* fetch associative array and display user names in a table*/
     while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["user_id"] . "</td></tr>";
+        echo "<option value='" . $row['user_id'] . "'>" . $row['user_id'] . "</option>";
     }
 
     /* free result set */
     $result->free();
-
-    echo "</table>";
 }
 
 /* close connection */
